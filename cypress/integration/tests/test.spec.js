b8ce2f19139a11c0f9ef64ea33895e076bfc8e1c
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-01-05 21:24:24 +0530

    added test cases and app file

---
+++ cypress/integration/tests/test.spec.js
@@ -0,0 +1,20 @@
+/// <reference types="Cypress"/>
+
+describe("testing Indian cities list", () => {
+    beforeEach(() => {
+      cy.visit("http://localhost:8080");
+    });
+    it("should check length of ordered list of cities", () => {
+      cy.get("ol").should("have.length", 16);
+    });
+    it("should check the key of ordered list", () => {
+      cy.get("ol").get("#location").should('be.visible');
+    });
+    it("should check the key of relative list", () => {
+      cy.get("ol").get("li").eq(0).get("#location1").should('be.visible');
+    });
+    it("should check the key of relative list", () => {
+        cy.get("ol").get("li").eq(15).get("#location16").should('be.visible');
+      });
+  });
+  
