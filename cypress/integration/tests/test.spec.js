3149c8515ab921a2b105800cc56de3f910f293bb
Suraj-Accio <suraj@acciojob.com>
2023-02-23 14:57:50 +0530

    template

--- cypress/integration/tests/test.spec.js
+++ cypress/integration/tests/test.spec.js
@@ -1,51 +1,16 @@
 /// <reference types="Cypress"/>
 
-describe("testing Indian cities list", () => {
+describe("test suite name", () => {
 
-  const cityList = [{ name: 'Goa', country: 'India' },
-    { name: 'Amsterdam', country: 'Netherlands' },
-    { name: 'New York', country: 'USA' },
-    { name: 'Darjeeling', country: 'India' },
-    { name: 'Tokyo', country: 'Japan' },
-    { name: 'Lonavala', country: 'India' },
-    { name: 'Brandenburg Gate', country: 'Germany' },
-    { name: 'Reichstag Building', country: 'Germany' },
-    { name: 'Museum Island', country: 'Germany' },
-    { name: 'Munnar', country: 'India' },
-    { name: 'Leh Ladakh', country: 'India' },
-    { name: 'Goa', country: 'India' },
-    { name: 'Agra', country: 'India' },
-    { name: 'Dalhousie', country: 'India' },
-    { name: 'Coorg', country: 'India' },
-    { name: 'Rome', country: 'Italy' },
-    { name: 'Milan', country: 'Italy' },
-    { name: 'Venice', country: 'Italy' },
-    { name: 'Varanasai', country: 'India' },
-    { name: 'Jaipur', country: 'India' },
-    { name: 'The Hofburg', country: 'Austria' },
-    { name: 'Belvedere Palace', country: 'Austria' },
-    { name: 'St. Stephen Cathedral', country: 'Austria' },
-    { name: 'Kahna National Park', country: 'India' },
-    { name: 'Amritsar', country: 'India' },
-    { name: 'Mussoorie', country: 'India' },
-    { name: 'Mount Abu', country: 'India' },
-    { name: 'Tirupati', country: 'India' },
-    ]
-
-    beforeEach(() => {
-      cy.visit("http://localhost:8080");
-    });
-    it("should check length of ordered list of cities", () => {
-      cy.get("ol").should("have.length", 1);
-    });
-    it("should check all Indian cities", () => {
-      cityList.forEach(city => {
-        const country = city.country
-        if(country == 'India') {
-          const name = city.name
-          cy.contains(name)
-        }
-      })
-    });
-  });
+  const baseURL = "http://localhost:8080";
   
+  beforeEach(() => {
+    cy.visit(baseURL);
+  });
+
+  it("test case name", () => {
+
+
+  });
+
+});
